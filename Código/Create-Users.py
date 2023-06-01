751e55b6f9e39a3438a65d09c3e9a215f90a39e8
Chris Pereira <christopher.gpereira06@gmail.com>
2023-06-01 17:48:42 -0300

    Adm on and configure pw
    This change created the Windows passwords setting and create .bat to adm on

--- Código/Create-Users.py
+++ Código/Create-Users.py
@@ -1,6 +1,3 @@
-"""
-CFM = Confirmation
-"""
 import os 
 import subprocess
 import getpass
@@ -12,28 +9,34 @@ os.system ('cls')
 def createUser ():
     firstName = input ("Qual o seu primeiro nome: ").capitalize()
     lastName = input ("Qual o seu sobrenome: ").capitalize()
-    cfmName = input (f"\033[1m{firstName} {lastName} \033[0m \nO seu nome está correto? (s/n) ")
-    while (cfmName != "s"):
+    cfmName = input (f"O nome: \033[1m{firstName} {lastName}\033[0m, está correto? (s/n)   \n")
+    while cfmName != "s":
         firstName = input ("Reescreva o seu primeiro nome: ").capitalize()
-        lastName = input ("Reescreva o seu sobrenome: ").capitalize()
-        os.system ("cls")
-        cfmName = input (f"\033[1m{firstName} {lastName} \033[0m \nO seu nome está correto? (s/n) ")
-        os.system ("cls")
+        lastName = input ("Reescreva o seu sobrenome: ").capitalize()    
+        cfmName = input (f"O nome: \033[1m{firstName} {lastName}\033[0m, está correto? (s/n)   \n")
 
     subprocess.run (f"powershell.exe New-LocalUser -Name {firstName} -Password (ConvertTo-SecureString -String '123@Faciap' -AsPlainText -Force) -FullName '{firstName} {lastName}' -Description 'Agente de Registro'")
     print (f"O usuário {firstName} {lastName} foi criado com sucesso!")
 
+def search_docs(name, directory):
+    results = []  
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if name in file:
+                results.append(os.path.join(root, file)) 
+
+    return results
+
 def cfgPw ():
-    print (os.system('net localgroup administradores'))
-    adm = input ('Qual usuário ADMINISTRADOR irá usar? \n')
-    user = getpass.getuser()
-    os.system (f'runas /user:{adm} "C:\\Users\\{user}\\Desktop\\teste\\CfgPwRequirements.bat"')
+
+    os.system (f'runas /user:"Administrador" "{search}"')
+    ### PRECISA CRIAR UM ZIP PARA O ARQUIVO .BAT
 
 
 
 ### CONFIRMAÇÃO / CONFIGURAÇÃO DO SISTEMA OPERACIONAL
-print ("-" * 30)
-print(' ' * 10, 'CONFIRMAÇÃO DO SISTEMA OPERACIONAL!!')
+print ('-' * 30)
+print (' ' * 10, 'CONFIRMAÇÃO DO SISTEMA OPERACIONAL!! \n')
 
 os.system ('systeminfo | findstr /B /C:"Nome do sistema operacional"')
 os.system ('systeminfo | findstr /B /C:"OS Name"')
@@ -45,31 +48,33 @@ if cfmPro != "s":
     print ("Para que as configurações possam ser realizadas é necessario que a versão do Windows seja Professional")
     os.system ('pause')
     exit ()
-print (linha, '\n')
-
-print ("-" * 30)
-
+print('\n')
 ### CRIAÇÃO DOS USUARIOS
-print ("-" * 30)
-print(' ' * 10, 'CRIAÇÃO DE USUÁRIOS!!')
+print ('-' * 30)
+print ('-' * 30)
+print('\n')
+print(' ' * 10, 'CRIAÇÃO DE USUÁRIOS!! \n')
 createUser ()
 moreUser = input ("Deseja adicionar mais usuários? (s/n) \n")
 
-while (moreUser == 's'):
-    createUser ()
+while moreUser == 's':
+    createUser()
     moreUser = input ("Deseja adicionar mais usuários? (s/n) \n")
     
 
 admUser = input ("Deseja criar usuário administrador? (s/n) \n")
 if admUser == "s":
-    subprocess.run ("powershell.exe New-LocalUser -Name 'Ar Faciap' -Password (ConvertTo-SecureString -String 'arfaciap@123' -AsPlainText -Force) -FullName 'Ar Faciap' -Description 'Ar Faciap'")
+    subprocess.run ("powershell.exe New-LocalUser -Name 'Ar Faciap' -Password (ConvertTo-SecureString -String arfaciap@123 -AsPlainText -Force) -FullName 'Ar Faciap' -Description 'Ar Faciap'")
     subprocess.run ("powershell.exe Add-LocalGroupMember -Group 'Administradores' -Member 'Ar Faciap'")
     os.system ('pause')
-print ("-" * 30)
+
 ### CONFIGURAÇÃO DOS USUARIOS 
-print ("-" * 30)
+
+print("Favor rodar script de habilitação do usuario ADMINISTRADOR!!")
+os.system('pause')
+search = ', '.join(search_docs('CfgPwRequirements.bat', 'C:\\'))
 print(' ' * 10, 'CONFIGURAÇÃO DE POLÍTICAS DE USUÁRIOS!!')
 cfgPw ()
-print ("-" * 30)
+
 print ("Configuração realizada com sucesso!!")
 os.system ('pause')
